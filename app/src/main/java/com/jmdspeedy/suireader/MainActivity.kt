59dce1f7dd8080db33489d988eadd8ed9e214937
jmdspeedy <Jmdimesdesigns@gmail.com>
2025-10-20 18:55:29 +0900

    Improve scan view UI and add dimming

--- app/src/main/java/com/jmdspeedy/suireader/MainActivity.kt
+++ app/src/main/java/com/jmdspeedy/suireader/MainActivity.kt
@@ -15,7 +15,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.view.MotionEvent
 import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.ImageView
